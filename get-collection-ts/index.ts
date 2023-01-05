82df695dfa94a06b3a2569e7fbf9f66e4c171162
Thomas Gehrmann <s6thgehr@gmail.com>
2023-01-05 11:50:07 +0100

    Update to latest @solana and @metaplex packages

--- get-collection-ts/index.ts
+++ get-collection-ts/index.ts
@@ -1,83 +1,88 @@
-import { Connection, PublicKey } from "@solana/web3.js";
-import { programs } from "@metaplex/js";
+import { ConfirmedSignatureInfo, Connection, PublicKey } from "@solana/web3.js";
 import fs from "fs";
-import { MetadataData } from "@metaplex-foundation/mpl-token-metadata";
-const {
-  metadata: { Metadata }
-} = programs;
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 
 async function main() {
-  // Get command line arguments
-  const args = process.argv.slice(2, 4)
+    // Get command line arguments
+    const args = process.argv.slice(2, 4);
 
-  let connection = new Connection(args[1] || "https://api.metaplex.com", "confirmed");
-  let collection_id = new PublicKey(args[0])
-  let metaplexProgramId = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
-
-  console.log("Getting signatures...");
-  let allSignatures = [];
-
-  // This returns the first 1000, so we need to loop through until we run out of signatures to get.
-  let signatures = await connection.getSignaturesForAddress(collection_id);
-  allSignatures.push(...signatures);
-  do {
-    let options = {
-      before: signatures[signatures.length - 1].signature
-    };
-    signatures = await connection.getSignaturesForAddress(
-      collection_id,
-      options
+    let connection = new Connection(
+        args[1] || "https://api.metaplex.com",
+        "confirmed"
     );
+    let collection_id = new PublicKey(args[0]);
+    let metaplexProgramId = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
+
+    console.log("Getting signatures...");
+    let allSignatures: ConfirmedSignatureInfo[] = [];
+
+    // This returns the first 1000, so we need to loop through until we run out of signatures to get.
+    let signatures = await connection.getSignaturesForAddress(collection_id);
     allSignatures.push(...signatures);
-  } while (signatures.length > 0);
+    do {
+        let options = {
+            before: signatures[signatures.length - 1].signature,
+        };
+        signatures = await connection.getSignaturesForAddress(
+            collection_id,
+            options
+        );
+        allSignatures.push(...signatures);
+    } while (signatures.length > 0);
 
-  console.log(`Found ${allSignatures.length} signatures`);
-  let metadataAddresses = [];
-  let mintAddresses = new Set<string>();
+    console.log(`Found ${allSignatures.length} signatures`);
+    let metadataAddresses: PublicKey[] = [];
+    let mintAddresses = new Set<string>();
 
-  console.log("Getting transaction data...");
-  const promises = allSignatures.map((s) =>
-    connection.getTransaction(s.signature)
-  );
-  const transactions = await Promise.all(promises);
+    console.log("Getting transaction data...");
+    const promises = allSignatures.map((s) =>
+        connection.getTransaction(s.signature)
+    );
+    const transactions = await Promise.all(promises);
 
-  console.log("Parsing transaction data...");
-  for (const tx of transactions) {
-    if (tx) {
-      let programIds = tx!.transaction.message
-        .programIds()
-        .map((p) => p.toString());
-      let accountKeys = tx!.transaction.message.accountKeys.map((p) =>
-        p.toString()
-      );
+    console.log("Parsing transaction data...");
+    for (const tx of transactions) {
+        if (tx) {
+            let programIds = tx!.transaction.message
+                .programIds()
+                .map((p) => p.toString());
+            let accountKeys = tx!.transaction.message.accountKeys.map((p) =>
+                p.toString()
+            );
 
-      // Only look in transactions that call the Metaplex token metadata program
-      if (programIds.includes(metaplexProgramId)) {
-        // Go through all instructions in a given transaction
-        for (const ix of tx!.transaction.message.instructions) {
-          // Filter for setAndVerify or verify instructions in the Metaplex token metadata program
-          if (
-            (ix.data == "K" || ix.data == "S") &&
-            accountKeys[ix.programIdIndex] == metaplexProgramId
-          ) {
-            let metadataAddressIndex = ix.accounts[0];
-            let metadata_address =
-              tx!.transaction.message.accountKeys[metadataAddressIndex];
-            metadataAddresses.push(metadata_address);
-          }
+            // Only look in transactions that call the Metaplex token metadata program
+            if (programIds.includes(metaplexProgramId)) {
+                // Go through all instructions in a given transaction
+                for (const ix of tx!.transaction.message.instructions) {
+                    // Filter for setAndVerify or verify instructions in the Metaplex token metadata program
+                    if (
+                        (ix.data == "K" || ix.data == "S") &&
+                        accountKeys[ix.programIdIndex] == metaplexProgramId
+                    ) {
+                        let metadataAddressIndex = ix.accounts[0];
+                        let metadata_address =
+                            tx!.transaction.message.accountKeys[
+                                metadataAddressIndex
+                            ];
+                        metadataAddresses.push(metadata_address);
+                    }
+                }
+            }
         }
-      }
     }
-  }
 
-  const promises2 = metadataAddresses.map((a) => connection.getAccountInfo(a));
-  const metadataAccounts = await Promise.all(promises2);
-  for (const account of metadataAccounts) {
-    let metadata = await MetadataData.deserialize(account!.data);
-    mintAddresses.add(metadata.mint);
-  }
-  let mints: string[] = Array.from(mintAddresses);
-  fs.writeFileSync(`${collection_id}_mints.json`, JSON.stringify(mints));
+    const promises2 = metadataAddresses.map((a) =>
+        connection.getAccountInfo(a)
+    );
+    const metadataAccounts = await Promise.all(promises2);
+    for (const account of metadataAccounts) {
+        if (account) {
+            let metadata = await Metadata.deserialize(account!.data);
+            mintAddresses.add(metadata[0].mint.toBase58());
+        }
+    }
+    let mints: string[] = Array.from(mintAddresses);
+    fs.writeFileSync(`${collection_id}_mints.json`, JSON.stringify(mints));
 }
 
 main().then(() => console.log("Success"));
